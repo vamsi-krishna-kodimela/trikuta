516b1a1a834fe8bb72d1543f2fd6d3609ddebd0e
Vamsi Krishna Kodimela <55780006+vamsi-krishna-kodimela@users.noreply.github.com>
2025-03-11 03:33:50 +0000

    Add New Password COnfig

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import HomeScreen from "./views/HomeScreen";
 import "./css/main.css";
+import { Toaster } from "@/components/ui/sonner";
 
-createRoot(document.getElementById("root")!).render(<HomeScreen />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <HomeScreen />
+    <Toaster />
+  </>
+);
--- src/views/components/AddNewPassword.tsx
+++ src/views/components/AddNewPassword.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-const AddNewPassword = () => {
-  return <div>AddNewPassword</div>;
+type Props = {
+  onSave: (username: string, password: string, website: string) => void;
+};
+
+const AddNewPassword = ({ onSave }: Props) => {
+  const [website, setWebsite] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const isFormValid = website && password && username;
+  return (
+    <div className="w-full md:w-1/2 m-auto flex flex-col gap-4">
+      <Input
+        placeholder="Website"
+        value={website}
+        onChange={(e) => setWebsite(e.target.value)}
+      />
+      <Input
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <Input
+        type="password"
+        placeholder="Enter Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button
+        disabled={!isFormValid}
+        onClick={() => onSave(username, password, website)}
+      >
+        Save Securely
+      </Button>
+    </div>
+  );
 };
 
 export default AddNewPassword;
